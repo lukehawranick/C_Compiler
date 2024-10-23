bc13f7704e664c54b9b90b34ceea6086cc545190
DatManJer <jka00003@mix.wvu.edu>
2024-10-23 10:49:46 -0400

    Quick fix to the files

--- Atom.java
+++ Atom.java
@@ -1,6 +1,6 @@
 /**
- * @file Atoms.java
- * @brief An implementation of Parser.
+ * @file Atom.java
+ * @brief An implementation taking token inputs into atom outputs.
  * @authors Jeremy Appiah, Garrett Williams
  * @reviewers 
  * @date 10/23/2024
