9832d5f6394307f656427bda5b5232c2558f8e73
DatManJer <jka00003@mix.wvu.edu>
2024-10-23 10:53:58 -0400

    Quick fix to attribution

--- Atom.java
+++ Atom.java
@@ -1,6 +1,6 @@
 /**
  * @file Atom.java
- * @brief An implementation taking token inputs into atom outputs.
+ * @brief An implementation Atoms taken by Parser.
  * @authors Jeremy Appiah, Garrett Williams
  * @reviewers 
  * @date 10/23/2024
--- Parser.java
+++ Parser.java
@@ -1,6 +1,6 @@
 /**
  * @file Atoms.java
- * @brief An implementation of Parser.
+ * @brief An implementation of Parser: taking token inputs into atom outputs.
  * @authors Jeremy Appiah, Garrett Williams
  * @reviewers 
  * @date 10/23/2024
