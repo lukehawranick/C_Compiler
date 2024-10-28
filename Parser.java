a3540181abe5a76178a359fe0cc3edb80a43c1f2
DatManJer <jka00003@mix.wvu.edu>
2024-10-28 15:15:09 -0400

    Parser, initial code

--- Parser.java
+++ Parser.java
@@ -6,3 +6,47 @@
  * @date 10/23/2024
  */
 
+ import java.util.List;
+
+public class Parser {
+    private List<String> input;
+    private int position;
+
+    public Parser(List<String> input) {
+        this.input = input;
+        this.position = 0;
+    }
+
+    private boolean accept(String terminal) {
+        if (position < input.size() && input.get(position).equals(terminal)) {
+            position++;
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressWarnings("unused")
+    private void expect(String terminal) {
+        if (!accept(terminal)) {
+            throw new RuntimeException("Syntax error: expected " + terminal);
+        }
+    }
+
+    public void parse() {
+        position = 0;
+        stmt();
+        if (position == input.size()) {
+            System.out.println("Parsing successful");
+        } else {
+            throw new RuntimeException("Syntax error: unexpected tokens at end of input");
+        }
+    }
+
+    private void stmt() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stmt'");
+    }
+}
+
+//subject to change; this is only how I interpreted the pseudo; added supress warning to 
+//identify my throught process
