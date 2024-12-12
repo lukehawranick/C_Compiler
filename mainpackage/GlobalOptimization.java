13873944dc403c15014ec46c2131e3585a4d5b57
Mallory88 <mka00009@mix.wvu.edu>
2024-12-12 15:03:40 -0500

    Update GlobalOptimization.java

--- mainpackage/GlobalOptimization.java
+++ mainpackage/GlobalOptimization.java
@@ -7,7 +7,25 @@ package mainpackage;
  * @date 12/12/2024
  */
 
+ import java.util.Collections;
+ import java.util.List;
+
 public class GlobalOptimization{
+	private final List<Atom> input;
+	private final List<Atom> output;
+
+	public GlobalOptimization (List<Atom> atoms) {
+		input = atoms;
+		output = optimize(input);
+	}
+
+	private List<Atom> optimize (List<Atom> input) {
+		return null;
+	}
+
+	public List<Atom> getOutput() {
+		return Collections.unmodifiableList(output);
+	}
 	//take in list of atoms
 	//search through list for jmp to find if lbl is after, if not delete code in between
 	//modify list of atoms
