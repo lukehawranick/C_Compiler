602244b4f711e177df40a6d3d52f61d26b2178ad
saraack10 <sna00008@mix.wvu.edu>
2024-12-12 15:05:15 -0500

    Update GlobalOptimization.java

--- mainpackage/GlobalOptimization.java
+++ mainpackage/GlobalOptimization.java
@@ -1,11 +1,4 @@
 package mainpackage;
-/**
- * @file GlobalOptimization.java
- * @brief An implementation of Global Optimization to delete unreachable code
- * @authors Sara Ackerman
- * @reviewers 
- * @date 12/12/2024
- */
 
  import java.util.Collections;
  import java.util.List;
