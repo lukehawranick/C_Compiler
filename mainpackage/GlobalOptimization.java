447588b752ba5080168d314f7de541cb6ef54cab
saraack10 <sna00008@mix.wvu.edu>
2024-12-16 18:52:01 -0500

    Worked on optimize method

--- mainpackage/GlobalOptimization.java
+++ mainpackage/GlobalOptimization.java
@@ -13,7 +13,21 @@ public class GlobalOptimization{
 	}
 
 	private List<Atom> optimize (List<Atom> input) {
-		return null;
+		for (int i = 0; i < input.size(); i++) {  //for each atom in list of atom
+			Atom a = input.get(i);
+			if (a.getOperand(0) == JMP) {  //if atom is a jmp atom, not sure how to code this
+				while (i + 1 < input.size()) { //check each atom in list after jmp atom until lbl atom is found
+					Atom nextAtom = input.get(i + 1);
+					if (nextAtom.getOperand(1) != LBL) {// if next atom is not a lbl atom, not sure how to code this
+						input.remove(i + 1);//remove that atom from the list
+					}  
+					else {
+						break; //get out of while loop
+					}
+				}
+			}
+		}
+		return input;
 	}
 
 	public List<Atom> getOutput() {
