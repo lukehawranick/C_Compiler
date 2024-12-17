75d3897a8b8a4370163db50b80c0d320934d30b9
saraack10 <sna00008@mix.wvu.edu>
2024-12-16 22:27:49 -0500

    Added GlobalOptimization to Compiler

--- mainpackage/GlobalOptimization.java
+++ mainpackage/GlobalOptimization.java
@@ -14,18 +14,18 @@ public class GlobalOptimization{
 		output = optimize(input);
 	}
 
- private List<Atom> optimize(List<Atom> input) {
+ public static List<Atom> optimize(List<Atom> input) {
         List<Atom> optimizedList = new ArrayList<>(input); // Create a modifiable copy of the list
         Iterator<Atom> iterator = optimizedList.iterator();
         int index = 0;
 
         while (iterator.hasNext()) {
             Atom atom = iterator.next();
-            if (atom.getOperand(0).equals("JMP")) { // Ensure proper comparison for "JMP"
+            if (atom.opcode.equals("JMP")) { // Ensure proper comparison for "JMP"
                 index++;
                 while (index < optimizedList.size()) {
                     Atom nextAtom = optimizedList.get(index);
-                    if (!nextAtom.getOperand(1).equals("LBL")) { // Ensure proper comparison for "LBL"
+                    if (!nextAtom.opcode.equals("LBL")) { // Ensure proper comparison for "LBL"
                         iterator.remove(); // Safe removal while iterating
                     } else {
                         break; // Exit the loop when an LBL is found
@@ -41,8 +41,4 @@ public class GlobalOptimization{
 	public List<Atom> getOutput() {
 		return Collections.unmodifiableList(output);
 	}
-	//take in list of atoms
-	//search through list for jmp to find if lbl is after, if not delete code in between
-	//modify list of atoms
-	//output new list
 }
